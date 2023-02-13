8362636393c2b5662a8ff92f56a752e86bf8643a
ClementAver <clement.aver@yahoo.fr>
2023-02-13 12:11:43 +0100

    Refactors listboxes creation into Search class.

--- public/js/pages/index.js
+++ public/js/pages/index.js
@@ -1,31 +1,22 @@
+// data > données métier.
 let instanciatedRecipes = [];
-let sortedRecipes = [];
-let sortedIngredients = [];
-let sortedAppliances = [];
 recipes.forEach((recipe) => instanciatedRecipes.push(new Recipe(recipe)));
-console.log(instanciatedRecipes);
 
-function createsfilterListboxes() {
-  //=//| filter division |\\=\\
-  let filterDivision = document.querySelector(".filters");
-
-  let ingredients = new Listbox({ label: "Ingrédients", placeholder: "Rechercher un ingrédient", id: "ingredients", options: ["Lait de coco", "Jus de citron", "Tomate"], color: "#3282f7" });
-  let ingredientsContainer = ingredients.createListboxNode();
-  let appliances = new Listbox({ label: "Appareils", placeholder: "Rechercher un appareil", id: "appliances", options: ["Lait de coco", "Jus de citron", "Tomate"], color: "#68d9a4" });
-  let appliancesContainer = appliances.createListboxNode();
-  let utensils = new Listbox({ label: "Ustensiles", placeholder: "Rechercher un ustensile", id: "utensils", options: ["Lait de coco", "Jus de citron", "Tomate"], color: "#ed6454" });
-  let utensilsContainer = utensils.createListboxNode();
-
-  filterDivision.appendChild(ingredientsContainer);
-  filterDivision.appendChild(appliancesContainer);
-  filterDivision.appendChild(utensilsContainer);
-}
+// console.log(instanciatedRecipes);
 
 function init() {
+  /*
+  // test unit - start
+  console.log("----> data :");
+  console.log(recipes);
+  console.log("----> instanciated recipes from data :");
+  console.log(instanciatedRecipes);
+  // test unit - end
+*/
+  // instanciates a new Search class...
   let search = new Search(instanciatedRecipes);
+  // ...and calls his search fonction.
   search.search();
-  console.log(sortedRecipes);
-  createsfilterListboxes();
 
   // écouteur de ma barre de recherche principale ici,
   // mais les écouteurs des tags dans les tags, où sera appelée la méthode search de l'instance de search.
--- public/js/utils/Search.js
+++ public/js/utils/Search.js
@@ -1,6 +1,7 @@
 class Search {
   constructor(recipes) {
     this._recipes = recipes;
+    this._sortedRecipes = [];
     this._searchBar = "";
     this._ingredients = [];
     this._appliances = [];
@@ -8,7 +9,49 @@ class Search {
   }
 
   search() {
-    // le tri avec la search bar
-    instanciatedRecipes.forEach((instanciatedRecipe) => sortedRecipes.push(instanciatedRecipe));
+    // fills the tags arrays with sorted recipes datas (1st line "this._recipes" to be replaced with "this._sortedRecipes").
+    this._recipes.forEach((recipe) => {
+      // fills this._ingredients.
+      recipe._ingredients.forEach((ingredient) => {
+        if (this._ingredients.indexOf(ingredient.ingredient) === -1) {
+          this._ingredients.push(ingredient.ingredient);
+        }
+      });
+      // fills this._appliances.
+      if (this._appliances.indexOf(recipe._appliance) === -1) {
+        this._appliances.push(recipe._appliance);
+      }
+      // fills this._utensils.
+      recipe._utensils.forEach((utensil) => {
+        if (this._utensils.indexOf(utensil) === -1) {
+          this._utensils.push(utensil);
+        }
+      });
+    });
+
+    // test unit - start
+    console.log("----> sorted recipes from instanciated ones :");
+    console.log(this._recipes);
+    console.log("----> sorted ingredients from sorted recipes :");
+    console.log(this._ingredients);
+    console.log("----> sorted appliances from sorted recipes :");
+    console.log(this._appliances);
+    console.log("----> sorted utensils from sorted recipes :");
+    console.log(this._utensils);
+    // test unit - end
+
+    //=//| filter division |\\=\\
+    let filterDivision = document.querySelector(".filters");
+
+    let ingredients = new Listbox({ label: "Ingrédients", placeholder: "Rechercher un ingrédient", id: "ingredients", options: this._ingredients, color: "#3282f7" });
+    let ingredientsContainer = ingredients.createListboxNode();
+    let appliances = new Listbox({ label: "Appareils", placeholder: "Rechercher un appareil", id: "appliances", options: this._appliances, color: "#68d9a4" });
+    let appliancesContainer = appliances.createListboxNode();
+    let utensils = new Listbox({ label: "Ustensiles", placeholder: "Rechercher un ustensile", id: "utensils", options: this._utensils, color: "#ed6454" });
+    let utensilsContainer = utensils.createListboxNode();
+
+    filterDivision.appendChild(ingredientsContainer);
+    filterDivision.appendChild(appliancesContainer);
+    filterDivision.appendChild(utensilsContainer);
   }
 }
