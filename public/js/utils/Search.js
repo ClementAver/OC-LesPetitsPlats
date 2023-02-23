c15efce703c80d393542cda3b64ce8cedf44ab74
ClementAver <clement.aver@yahoo.fr>
2023-02-23 10:59:52 +0100

    refactors the search() class method to initialize a set and remove recipes that don't match the conditions.

--- public/js/utils/Search.js
+++ public/js/utils/Search.js
@@ -8,110 +8,52 @@ export default class Search {
   search() {
     console.clear();
 
-    this._sortedRecipes = new Set(this._recipes);
+    let sortedRecipes = new Set(this._recipes);
 
-    // if the searchBarValue set contains something :
     if (searchBarValue.size > 0) {
-      let temp = this._sortedRecipes;
-      let tempB = new Set();
-
-      temp.forEach((recipe) => {
+      sortedRecipes.forEach((recipe) => {
         // searchBarValue.values().next().value returns the first value of an iterator based on the searchBarValue set.
         let noCaseTag = new RegExp(searchBarValue.values().next().value, "i");
         // matches if regex tests true on either name, ingredients OR description AND isn't already sorted.
-        recipe.nameMatches(noCaseTag) || recipe.ingredientsMatches(noCaseTag) || recipe.descriptionMatches(noCaseTag) ? tempB.add(recipe) : false;
+        recipe.nameMatches(noCaseTag) || recipe.ingredientsMatches(noCaseTag) || recipe.descriptionMatches(noCaseTag) ? true : sortedRecipes.delete(recipe);
       });
-      temp = tempB;
-      this._sortedRecipes = temp;
     }
 
-    /* --new version to be tested--
-    if (searchBarValue.size > 0) {
-      let temp = new Set();
-      this._sortedRecipes.forEach((recipe) => {
-        // searchBarValue.values().next().value returns the first value of an iterator based on the searchBarValue set.
-        let noCaseTag = new RegExp(searchBarValue.values().next().value, "i");
-        // matches if regex tests true on either name, ingredients OR description AND isn't already sorted.
-        recipe.nameMatches(noCaseTag) || recipe.ingredientsMatches(noCaseTag) || recipe.descriptionMatches(noCaseTag) ? temp.add(recipe) : false;
-      });
-      this._sortedRecipes = temp;
-    }
-    */
-
-    // if the ingredientsTags array contains at least one tag :
     if (ingredientsTags.size > 0) {
-      let temp = this._sortedRecipes;
       ingredientsTags.forEach((tag) => {
-        let tempB = new Set();
-        temp.forEach((recipe) => {
-          recipe._ingredients.forEach((ingredient) => {
-            ingredient.ingredient.includes(tag) ? tempB.add(recipe) : false;
-          });
-        });
-        temp = tempB;
-      });
-      this._sortedRecipes = temp;
-    }
-
-    /* --new version to be tested--
-    if (ingredientsTags.size > 0) {
-      let temp = new Set();
-      ingredientsTags.forEach((tag) => {
-        this._sortedRecipes.forEach((recipe) => {
-          recipe._ingredients.forEach((ingredient) => {
-            ingredient.ingredient.includes(tag) ? temp.add(recipe) : false;
-          });
+        sortedRecipes.forEach((recipe) => {
+          if (!recipe._ingredients.some((ingredient) => ingredient.ingredient.includes(tag))) {
+            sortedRecipes.delete(recipe);
+          }
         });
       });
-      this._sortedRecipes = temp;
     }
-    */
 
-    // if the appliancesTags array contains at least one tag :
     if (appliancesTags.size > 0) {
-      let temp = new Set();
       appliancesTags.forEach((tag) => {
-        this._sortedRecipes.forEach((recipe) => {
-          recipe._appliance.includes(tag) ? temp.add(recipe) : false;
+        sortedRecipes.forEach((recipe) => {
+          if (!recipe._appliance.includes(tag)) {
+            sortedRecipes.delete(recipe);
+          }
         });
       });
-      this._sortedRecipes = temp;
     }
 
-    // if the utensilsTags array contains at least one tag :
     if (utensilsTags.size > 0) {
-      let temp = this._sortedRecipes;
       utensilsTags.forEach((tag) => {
-        let tempB = new Set();
-        temp.forEach((recipe) => {
-          recipe._utensils.forEach((utensil) => {
-            utensil.includes(tag) ? tempB.add(recipe) : false;
-          });
-        });
-        temp = tempB;
-      });
-      this._sortedRecipes = temp;
-    }
-
-    /* --new version to be tested--
-    if (utensilsTags.size > 0) {
-      let temp = new Set();
-      utensilsTags.forEach((tag) => {
-        this.sortedRecipes.forEach((recipe) => {
-          recipe._utensils.forEach((utensil) => {
-            utensil.includes(tag) ? temp.add(recipe) : false;
-          });
+        sortedRecipes.forEach((recipe) => {
+          if (!recipe._utensils.some((utensil) => utensil.includes(tag))) {
+            sortedRecipes.delete(recipe);
+          }
         });
       });
-      this._sortedRecipes = temp;
     }
-    */
 
     // test unit - start
-    console.log("----> this._sortedRecipes :");
-    console.log(this._sortedRecipes);
+    console.log("----> sortedRecipes :");
+    console.log(sortedRecipes);
     // test unit - end
 
-    return this._sortedRecipes;
+    return sortedRecipes;
   }
 }
